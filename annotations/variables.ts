6c50d7f2f59cf2dac13d2176a537ffec375e0b61
Rus Hughes <russell.hughes@gmail.com>
2020-07-15 16:22:15 +0200

    object literal annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -3,4 +3,20 @@ let speed: string = 'fast';
 let hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
+
 let now: Date = new Date();
+
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 4, 5];
+let truths: boolean[] = [true, false, true];
+
+// classes
+
+class Car {}
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
