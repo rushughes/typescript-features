02c5f87aadea1b20df6ceebba8d432bea43a9bb6
Rus Hughes <russell.hughes@gmail.com>
2020-07-15 16:47:48 +0200

    annotating a delayed initialisation

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -34,3 +34,13 @@ typeInference = 1;
 const json = '{"x": 10, "y": 20}';
 const coords: { x: number; y: number } = JSON.parse(json);
 console.log(coords);
+
+// 2) when we declare a variable on one line and initialise it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
