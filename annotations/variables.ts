18bd2267489cffd0d9eb5c2ddf195e6512019ea9
Rus Hughes <russell.hughes@gmail.com>
2020-07-16 09:06:12 +0200

    destructuring with annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -85,3 +85,26 @@ const throwOtherError = (message: string): string => {
     return message;
   }
 };
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = (forecast: { date: Date; weather: string }): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+const logWeatherDestructed = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
