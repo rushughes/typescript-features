df486f46c1b455e4b3c4d22a066fcb95658f7ee0
Rus Hughes <russell.hughes@gmail.com>
2020-07-16 17:08:13 +0200

    array annotations

---
+++ annotations/arrays.ts
@@ -0,0 +1,7 @@
+const carMarkers = ['ford', 'toyota', 'chevy'];
+const bikeMakers: string[] = [];
+
+const dates = [new Date(), new Date(), new Date()];
+
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
+const bikeByMake: string[][] = [];
