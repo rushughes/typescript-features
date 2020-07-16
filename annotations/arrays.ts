d8aca8f5e06bc87d675b937d9a524c1f73a5fb5c
Rus Hughes <russell.hughes@gmail.com>
2020-07-16 17:17:55 +0200

    More typed arrays!

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -1,7 +1,19 @@
-const carMarkers = ['ford', 'toyota', 'chevy'];
+const carMakers = ['ford', 'toyota', 'chevy'];
 const bikeMakers: string[] = [];
 
 const dates = [new Date(), new Date(), new Date()];
 
 const carsByMake = [['f150'], ['corolla'], ['camaro']];
 const bikeByMake: string[][] = [];
+
+// Help with inference when extracting values
+
+const car1 = carMakers[0];
+const myCar = carMakers.pop();
+
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+const importantDates = [new Date(), '2030-10-10'];
+const unImportantDates: (Date | string)[] = [];
