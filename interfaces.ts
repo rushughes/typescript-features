745cd4a9ea9b55dd2cad9320a2fe1462567c84e1
Rus Hughes <russell.hughes@gmail.com>
2020-07-17 08:37:47 +0200

    adding interface annotations

---
+++ interfaces.ts
@@ -0,0 +1,36 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: true,
+};
+
+const NewBeetle: Vehicle = {
+  name: 'beetle',
+  year: 2018,
+  broken: false,
+};
+
+const printVehicle = (vehicle: {
+  name: string;
+  year: number;
+  broken: boolean;
+}): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken: ${vehicle.broken}`);
+};
+
+const printVehicle2 = (vehicle: Vehicle): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken: ${vehicle.broken}`);
+};
+
+printVehicle(oldCivic);
+printVehicle2(oldCivic);
