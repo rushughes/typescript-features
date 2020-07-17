2d0000929eb0e97909d3d5e697ee6d95c4a0511a
Rus Hughes <russell.hughes@gmail.com>
2020-07-17 08:41:30 +0200

    adding object and functions to interface declarations

--- interfaces.ts
+++ interfaces.ts
@@ -1,25 +1,33 @@
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
 const NewBeetle: Vehicle = {
   name: 'beetle',
-  year: 2018,
+  year: new Date(),
   broken: false,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
 const printVehicle = (vehicle: {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  summary(): string;
 }): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
